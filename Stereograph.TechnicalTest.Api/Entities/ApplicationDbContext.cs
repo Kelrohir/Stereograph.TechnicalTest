10c7df3d59bf24fcf78bd9442f333efba4ce9d2d
Kelrohir <cobigoxavier@gmail.com>
2023-05-29 16:18:39 +0200

    Ajoutez des fichiers projet.

---
+++ Stereograph.TechnicalTest.Api/Entities/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stereograph.TechnicalTest.Api.Entities;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
---
+++ Stereograph.TechnicalTest.Api/Models/Persons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace Stereograph.TechnicalTest.Api.Models;
+
+public class Persons
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+}
